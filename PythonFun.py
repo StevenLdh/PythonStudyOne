03138e752552d82847ba4f180629212de31da61c
lidehua <lidehuawork@163.com>
2018-11-05 11:40:24 +0800

    使用函数局部变量和全局变量的区别

--- PythonFun.py
+++ PythonFun.py
@@ -17,3 +17,15 @@ printinfo(70, 60, 50)
 sum=lambda arg1,arg2:arg1+arg2
 print("相加后的值为 :",sum(10,20))
 print("相加后的值为 :",sum(20,20))
+
+# 全局变量和局部变量
+total = 0  # 这是一个全局变量
+# 可写函数说明
+def sum(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2  # total在这里是局部变量.
+    print("函数内是局部变量 : ", total)
+    return total
+# 调用sum函数
+sum(10, 20);
+print("函数外是全局变量 : ", total)
