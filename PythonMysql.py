e0a16c0d286aa7e6a286c4afe3a4984facf83d35
Steven <lidehuawork@163.com>
2018-11-13 21:10:52 +0800

    commit

--- PythonMysql.py
+++ PythonMysql.py
@@ -21,13 +21,13 @@ for x in myresult:
 """
 import pymysql
 # 打开数据库连接
-db = pymysql.connect("localhost", "root", "123456", "cc_erp")
+db = pymysql.connect("localhost", "root", "qwert", "world")
 
 # 使用 cursor() 方法创建一个游标对象 cursor
 cursor = db.cursor()
 
 # 使用 execute()  方法执行 SQL 查询
-cursor.execute("SELECT * FROM bas_account")
+cursor.execute("SELECT * FROM city")
 
 # 使用 fetchone() 方法获取单条数据.
 data = cursor.fetchone()
