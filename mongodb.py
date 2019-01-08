1831b76b8eeb5ab867c5f3d8ff42c8f4e82a037a
lidehua <lidehuawork@163.com>
2019-01-08 20:57:51 +0800

    mongodb test

---
+++ mongodb.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# 引用插件
+import pymongo
+'''
+判断数据库是否存在
+'''
+myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+dblist = myclient.list_database_names()
+if "stevendb" in dblist:
+    print("数据库已存在")
+# 访问数据库
+mydb = myclient["stevendb"]
+# 指定表
+mycol = mydb["runoob"]
+# 添加数据（插入单行）
+'''
+mydict = { "name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com" }
+print(mycol.insert_one(mydict).inserted_id)
+'''
+
+# 添加数据（插入多行）
+'''
+mylist = [
+  { "name": "Taobao", "alexa": "100", "url": "https://www.taobao.com" },
+  { "name": "QQ", "alexa": "101", "url": "https://www.qq.com" },
+  { "name": "Facebook", "alexa": "10", "url": "https://www.facebook.com" },
+  { "name": "知乎", "alexa": "103", "url": "https://www.zhihu.com" },
+  { "name": "Github", "alexa": "109", "url": "https://www.github.com" }
+]
+print(mycol.insert_many(mylist).inserted_ids)
+
+'''
+# 修改数据（单个修改）
+'''
+myquery = { "alexa": "10000" }
+newvalues = { "$set": { "alexa": "12345" } }
+mycol.update_one(myquery,newvalues)
+'''
+# 修改数据（多行修改）
+'''
+myquery = { "name":{ "$regex": "^F" } }
+newvalues = { "$set": { "name": "steven" } }
+mycol.update_many(myquery,newvalues)
+'''
+# 删除数据（单行删除）
+'''
+myquery={"name":"QQ"}
+mycol.delete_one(myquery)
+'''
+# 删除数据（多行删除）
+'''
+myquery = { "name":{ "$regex": "^F" } }
+mycol.delete_many(myquery)
+'''
+# 查询数据
+'''
+for item in mycol.find().sort("alexa"):
+    print(item)
+'''
+
+# 查询条件 AND OR
+'''
+print("---------AND条件（where alexa='100' and name='Taobao' ）-----")
+for item in mycol.find({"alexa":"100","name":"Taobao"}):
+    print(item)
+print("---------OR条件（where alexa='100' or name='Taobao' ）-----")
+for item in mycol.find({"$or": [{"alexa":"100"},{"name":"Taobao"}]}):
+    print(item)
+'''
+
+
+
